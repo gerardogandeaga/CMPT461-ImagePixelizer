a623494c8b8ac8bb688cb62abb7599e6aa66ca24
Gerardo Gandeaga <gerardogandeaga@gmail.com>
2022-04-14 22:47:37 -0700

    added setup script template

---
+++ krita/setup.sh
@@ -0,0 +1,7 @@
+# You can wget the file and put it in
+# $REPO/krita/my_extension/core/xt_consistency/models/rgb2normal_consistency.pth
+
+# From XTC download file
+# wget https://drive.switch.ch/index.php/s/QPvImzbbdjBKI5P/download
+# unzip download
+# rm download
